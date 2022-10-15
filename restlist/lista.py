a29b97c2758cc6154986666920de34483dedef60
josemiguelmo2 <josemiguel.moreno2@alu.uclm.es>
2022-10-15 19:57:53 +0200

    inicio persistencia

--- restlist/lista.py
+++ restlist/lista.py
@@ -10,32 +10,35 @@ class Dir_BD:
     def __init__(self):
         self.storage = []
 
-    def append(self, element):
+    def create_dir(self, element):
         '''Incluye un nuevo elemento al final de la lista'''
         self.storage.append(element)
 
-    def element_at(self, index):
+    def remove_dir(self, index):
         '''Retorna el elemento en la posicion "index" de la lista'''
         return self.storage[index]
 
-    def exists(self, element):
+    def add_user_readable(self, element):
         '''Retorna si un elemento dado esta o no en la lista'''
         return element in self.storage
 
-    def remove(self, element, all_occurrences=False):
+    def remove_user_readable(self, element, all_occurrences=False):
         '''Elimina un elemento de la lista. Puede eliminar todas sus apariciones'''
         self.storage.remove(element)
         while (element in self.storage) and all_occurrences:
             self.storage.remove(element)
 
-    def count(self, element):
+    def add_user_writeable(self, element):
         '''Cuenta el numero de veces que aparece un elemento en la lista'''
         return self.storage.count(element)
 
-    def len(self):
+    def remove_user_writeable(self):
         '''Devuelve la longitud de la lista'''
         return len(self.storage)
 
-    def wipe(self):
+    def add_dir(self):
         '''Vacia la lista'''
         self.storage = []
+
+    def remove_dir(self):
+        
